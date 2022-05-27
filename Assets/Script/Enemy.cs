2c7dd07efa97d00c1a0975663694ba0546275b45
宋惟嘉 <feifeige@feifeideMBP.lan>
2022-05-26 23:40:54 -0700

    first

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+    public int health;
+    public int damage;
+    public float flashTime;
+    private SpriteRenderer sr;
+    private Color originalColor;
+    // Start is called before the first frame update
+    public void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        originalColor=sr.color;
+
+
+        
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if(health<=0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        FlashColor(flashTime);
+    }
+    void FlashColor(float time)
+    {
+        sr.color=Color.red;
+        Invoke("ResetColor",time);
+    }
+    void ResetColor()
+    {
+        sr.color=originalColor;
+    }
+}
